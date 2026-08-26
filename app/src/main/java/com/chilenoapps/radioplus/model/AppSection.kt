d5dc9dfb10175f6bad31ad16c3a86d952121e941
oinavile2-max <oinavile2@gmail.com>
2026-08-25 22:43:25 -0300

    feat: adiciona app/src/main/java/com/chilenoapps/radioplus/model/AppSection.kt

---
+++ app/src/main/java/com/chilenoapps/radioplus/model/AppSection.kt
@@ -0,0 +1,10 @@
+package com.chilenoapps.radioplus.model
+
+enum class AppSection(val title: String, val symbol: String) {
+    RADIO("RÁDIO", "◉"),
+    MUSIC("MÚSICA", "♫"),
+    VIDEO("VÍDEO", "▶"),
+    ONLINE("ONLINE", "◎"),
+    MAPS("MAPAS", "◆"),
+    PHONE("TELEFONE", "☎")
+}
